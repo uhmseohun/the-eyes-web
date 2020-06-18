d3f1f1010c06cd198bc4a7a2b5c51ad6aed6c593
uhmseohun <uhmtotow@gmail.com>
2020-06-18 10:34:52 +0900

    feat: implement game playing

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -4,7 +4,15 @@
 import { Vue, Component } from 'vue-property-decorator';
 import { RecordRTCPromisesHandler } from 'recordrtc';
 
-@Component
+@Component({
+  filters: {
+    filterTime(value: number) {
+      const minutes = String(Math.floor(value / 60)).padStart(2, '0');
+      const seconds = String(value % 60).padStart(2, '0');
+      return `${minutes}:${seconds}`;
+    },
+  },
+})
 export default class Game extends Vue {
   stream!: any;
 
@@ -14,6 +22,10 @@ export default class Game extends Vue {
 
   contentType = ({ _boundary: b }: any) => `multipart/form-data; boundary=${b}`;
 
+  startTime!: number = new Date().getTime();
+
+  playingTime = 0;
+
   async created() {
     this.stream = await navigator.mediaDevices.getUserMedia({
       video: {
@@ -21,15 +33,34 @@ export default class Game extends Vue {
         height: { exact: 400 },
       },
     });
-
     this.$refs.video.srcObject = this.stream;
-
     this.recorder = new RecordRTCPromisesHandler(this.stream, {
       type: 'video',
     });
+
+    setInterval(() => {
+      const currentTime = new Date().getTime();
+      this.playingTime = Math.floor((currentTime - this.startTime) / 1000);
+    }, 1000);
+
+    await this.sleep(2000);
+
+    // eslint-disable-next-line
+    while (!await this.isEyeclosed());
+
+    const { $io } = Vue.prototype;
+    $io.emit('eyeClosed', this.playingTime);
+
+    this.$store.dispatch('leaveRoom');
+    this.$router.push({
+      path: '/game/finished',
+      params: {
+        playedTime: this.playingTime,
+      },
+    });
   }
 
-  async getResult() {
+  async isEyeclosed() {
     this.recorder.startRecording();
     await this.sleep(1000);
     await this.recorder.stopRecording();
@@ -49,5 +80,45 @@ export default class Game extends Vue {
 </script>
 
 <template>
-  <video ref="video" width="400" height="400" autoplay />
+  <div class="container">
+    <div class="section section-left">
+      <video class="webcam" ref="video" autoplay />
+    </div>
+    <div class="section section-right">
+      <div class="header">
+        <h1 class="title">지금 플레이 중</h1>
+        <h2 class="timer">⏰ {{ playingTime | filterTime }}</h2>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.section {
+  width: 40vw;
+  min-height: 550px;
+  display: unset;
+
+  &__left {}
+  &__right {}
+}
+
+.title, .timer {
+  display: inline;
+}
+.title {
+  margin-right: 15px;
+}
+
+.webcam {
+  display: none;
+  width: 550px;
+  height: 550px;
+}
+</style>
