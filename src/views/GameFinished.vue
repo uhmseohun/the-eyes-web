acc852b9414244f1299ea8331d1080ac6f7798b0
uhmseohun <uhmtotow@gmail.com>
2020-06-30 14:29:51 +0900

    feat: add share button and time filter

--- src/views/GameFinished.vue
+++ src/views/GameFinished.vue
@@ -1,7 +1,9 @@
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
+import KakaoSendButton from '@/components/KakaoSendButton.vue';
 
 @Component({
+  components: { KakaoSendButton },
   computed: {
     playedTime() {
       return this.$route.params.playedTime;
@@ -13,13 +15,27 @@ import { Vue, Component } from 'vue-property-decorator';
       throw new Error('잘못된 접근입니다. 메인 페이지로 돌아갑니다.');
     }
   },
+  filters: {
+    filterTime(value: number) {
+      const minutes = String(Math.floor(value / 60)).padStart(2, '0');
+      const seconds = String(value % 60).padStart(2, '0');
+      return `${minutes}분 ${seconds}초`;
+    },
+  },
 })
 export default class GameFinished extends Vue {}
 </script>
 
 <template>
   <div class="container">
-    {{ playedTime }}
+    <h1 class="form__title">⏰ 당신의 기록</h1>
+    <h2 class="form__description">{{ playedTime | filterTime }}나 버티셨네요! 🎉</h2>
+
+    <h1 class="form__title">⏰ 친구의 기록</h1>
+    <h2 class="form__description">{{ playedTime | filterTime }}나 버티셨네요! 🎉</h2>
+    <kakao-send-button>
+      카카오톡으로 친구에게 자랑하기
+    </kakao-send-button>
   </div>
 </template>
 
