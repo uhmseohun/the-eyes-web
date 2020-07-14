463ad1ca5ad4022111f83620f210ffc53d83d33b
uhmseohun <uhmtotow@gmail.com>
2020-07-14 12:30:55 +0900

    feat: change webapp url to real server

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ import { SwalOption } from '@/interfaces';
 export const VISION_SERVER_URL = 'http://localhost:5000';
 export const BACKEND_API_URL = 'http://localhost:3000';
 export const SOCKET_IO_URL = 'http://localhost:3000';
-export const WEBAPP_URL = 'http://localhost:8080';
+export const WEBAPP_URL = 'https://the-eyes.now.sh';
 export const KAKAO_SDK_KEY = '29631686dd2f203de7e6bc295659a4ea';
 export const GOOGLE_TRACKING_ID = 'UA-145834925-2';
 export const QRCODE_API_URI = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=';
